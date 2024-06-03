d4aa2c6985d2559b3e15febc04a68aab45f00b2a
arachn0id887 <vivaanjha@gmail.com>
2024-06-03 15:06:30 +0530

    Add files via upload

---
+++ waitlist.js
@@ -0,0 +1,55 @@
+const button = document.getElementById("contactbutton");
+const closebutton = document.getElementById("close");
+const form = document.getElementById("form");
+
+button.addEventListener("click", function(){
+  form.style.display = "block"
+})
+closebutton.addEventListener("click", function(){
+  form.style.display = "none"
+})
+
+function displayAlert(event) {
+  const name = document.getElementById("namebox").value;
+  const email = document.getElementById("emailbox").value;
+  const yes = document.getElementById("button1");
+  const no = document.getElementById("button2");
+
+  var data = "Here's what was received:" + "\n";
+  data += "Name: " + name.charAt(0).toUpperCase() + name.slice(1, name.indexOf(" ")) + " " + name.charAt(name.indexOf(" ") + 1).toUpperCase() + name.slice(name.indexOf(" ") + 2) + "\n";
+  data += "Email: " + email + "\n";
+  if(!name.includes(" ")){
+    window.alert("Please enter your full name.")
+  }
+  else if(yes.checked){
+    data += "Notifications: Yes"
+    window.alert(data)
+    form.style.display = "none";
+    name = "";
+    email = "";
+    yes.checked = false;
+    no.checked = false;
+  }
+  else if(no.checked){
+    data += "Notifications: No"
+    window.alert(data)
+    form.style.display = "none";
+    name = "";
+    email = "";
+    yes.checked = false;
+    no.checked = false;
+  }
+  else if(!yes.checked && !no.checked){
+    window.alert("Please select whether you would like to receive notifications or not.")
+  }
+  else{
+    window.alert(data)
+    form.style.display = "none";
+    name = "";
+    email = "";
+    yes.checked = false;
+    no.checked = false;
+  }
+  event.preventDefault();
+}
+
